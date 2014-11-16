861b2bcaed0ba797fa1ad0ffe99db5c7cc9cf7cc
Joe Burdick <josephdburdick@gmail.com>
2014-11-16 16:23:41 -0500

    Use http package to get weather JSON and update helper on select change event.

---
+++ client/layout.js
@@ -0,0 +1,19 @@
+if (Meteor.isClient){
+	Template.layout.helpers({
+		currentWeather: function(){
+			var city = Session.get('city');
+
+			Meteor.call('getWeather', city, function(error, results){
+				Session.set('weather', JSON.parse(results.content).weather[0].description);
+			});
+			return (Session.get('weather') + ' in ' + city).toUpperCase();	
+		}
+	});
+	Template.layout.events({
+		'change .cities': function(event, template){
+			Session.set('city', template.find('.cities').value)
+			// var city = event.currentTarget.value;
+			Session.get('currentWeather');
+		} 
+	});
+}
